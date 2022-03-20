bfb4616aca0e095dd2bbde070beb62629c13ff18
dasom <dasom228@gmail.com>
2022-03-20 18:51:20 +0900

    Setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,13 @@
-// import React, { FC, useCallback, useEffect, useState } from 'react'
 import React, { FC } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import WalletWeb3React from './routes/wallet-web3-react';
-// import { votingContract } from './web3Config'
-import Complete from './routes/complete'
-import Vote from './routes/vote';
-
-// 1. 투표를 안한경우 - vote 가능
-// 2. 투표를 한번 했고 24시간 지나지 않은 경우 - complete페이지 접근 가능
-// 3. 투표를 했고 24시간 지난 경우 - vote, complete페이지 접근 가능
+import Home from './routes/home'
 
 const App: FC = () => {
-  // const [isVoted, setIsVoted] = useState<boolean | null>(null)
-
-
-  // const checkVoted = useCallback(async (): Promise<void> => {
-  //   if (!account) return
-  //   try {
-  //     const result = await votingContract.methods.isVoted(account).call()
-  //     setIsVoted(result)
-  //   } catch(error) {
-  //     console.error(error)
-  //   }
-  // }, [account])
-
-  // useEffect(() => {
-  //   getAccount()
-  //   checkVoted()
-  // }, [checkVoted])
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<WalletWeb3React />} />
-        {/* <Route path="/vanilaWallet" element={<WalletConnetVanila />} /> */}
-        <Route path="/vote" element={<Vote />} />
-        <Route path="/complete" element={<Complete />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/routes/home.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+
+const Home: FC = () => {
+  // view
+  return (
+    <>
+      <p>Hello world</p>
+    </>
+  )
+}
+
+export default Home
