7dddb477edc7cb7bd44481687a6b52a1cd366351
dasom <dasom228@gmail.com>
2022-03-22 23:47:26 +0900

    Add key for iteration

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -52,7 +52,7 @@ const Header = () => {
           </div>
           <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
             {navigation.map((item) => (
-              <Link to={item.link} className="font-medium text-white hover:text-aid-blue transition-all duration-300">
+              <Link key={item.name} to={item.link} className="font-medium text-white hover:text-aid-blue transition-all duration-300">
                 {item.name}
               </Link>
             ))}
@@ -94,7 +94,7 @@ const Header = () => {
                 </div>
                 <div className="px-2 pt-2 pb-3 space-y-1 bg-white bg-opacity-90">
                   {navigation.map((item) => (
-                    <Link to={item.link} className="block px-3 py-2 rounded-md text-base font-medium text-gray-700">
+                    <Link key={item.name} to={item.link} className="block px-3 py-2 rounded-md text-base font-medium text-gray-700">
                       {item.name}
                     </Link>
                   ))}
