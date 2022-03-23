e649f3986b25f25bf68ffc46d95187af0e409ba9
dasom <dasom228@gmail.com>
2022-03-23 12:35:15 +0900

    Add prop

--- src/components/TitleBox.tsx
+++ src/components/TitleBox.tsx
@@ -6,11 +6,12 @@ interface TitleBoxProps {
   subTitle: string
   description?: string
   align: AlignType
+  color?: string
 }
 
-const TitleBox:FC<TitleBoxProps> = ({mainTitle, subTitle, description, align}) => {
+const TitleBox:FC<TitleBoxProps> = ({mainTitle, subTitle, description, align, color}) => {
   return (
-    <div className={`text-gray-800, text-${align} px-4 pb-6 md:pb-8`}>
+    <div className={`text-gray-800, text-${align} px-4 pb-6 md:pb-8 ${color ? 'text-' + color : ''}`}>
       <h2 className="title text-3xl tracking-tight font-extrabold md:text-5xl">{mainTitle}</h2>
       <p className="sub-title text-lg md:text-2xl font-normal mt-6 md:mt-8">{subTitle}</p>
       {description && <p className="sub-title text-base md:text-lg font-normal mt-6 md:mt-7">{description}</p>}
