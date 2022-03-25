0c7bb997c66efb8cf518c35ed6023d9f90fdce39
dasom <dasom228@gmail.com>
2022-03-25 22:58:10 +0900

    Add tailwindcss plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -70,15 +70,6 @@ module.exports = {
         })
       })
     }),
+    require('@tailwindcss/forms'),
   ],
 }
-
-// module.exports = {
-//   content: [
-//     "./src/**/*.{js,jsx,ts,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
