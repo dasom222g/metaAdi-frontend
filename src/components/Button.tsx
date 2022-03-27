f4c4c77a3572918adc92f4c6878998635661a129
dasom <dasom228@gmail.com>
2022-03-27 21:30:19 +0900

    Fix button style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,10 +6,10 @@ interface ButtonProps {
 
 const Button: FC<ButtonProps> = ({ title }) => {
   return (
-    <div className="mt-6 md:mt-8">
+    <div className="p-2">
       <button
         type="button"
-        className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base rounded-lg text-indigo-700 font-extrabold bg-indigo-100 hover:bg-indigo-300 md:py-4 md:text-lg md:px-10 transition-all duration-300">
+        className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base rounded-md text-indigo-700 font-extrabold bg-indigo-100 hover:bg-indigo-300 md:py-4 md:text-lg md:px-10 transition-all duration-300">
         {title}
       </button>
     </div>
--- src/data/style.tsx
+++ src/data/style.tsx
@@ -4,4 +4,5 @@ export const style = {
   innerContentInterval: 'py-2',
   roundBox: 'bg-aid-light-white rounded-lg text-center p-4 md:p-6 mx-auto shadow-2xl',
   roundContent: 'py-2 px-4 rounded overflow-hidden',
+  buttonOutInterval: '-mx-2',
 }
