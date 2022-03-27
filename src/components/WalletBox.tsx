6c5682907876f93e3f87f62f63772e7c54bb4555
dasom <dasom228@gmail.com>
2022-03-27 18:12:24 +0900

    Fix data

--- src/components/WalletBox.tsx
+++ src/components/WalletBox.tsx
@@ -1,22 +1,12 @@
 import React, { FC } from 'react'
-import { useTranslation } from 'react-i18next'
+// import { useTranslation } from 'react-i18next'
+import { walletList } from '../data/response'
 import { WalletType } from '../lib/type'
 import WalletBoxButton from './WalletBoxButton'
 
 const WalletBox: FC = () => {
-  const { t } = useTranslation()
-  const wallets: WalletType[] = [
-    {
-      id: 1,
-      type: 'META_MASK',
-      title: t('wallet.meta_mask'),
-    },
-    {
-      id: 2,
-      type: 'WALLET_CONNECT',
-      title: t('wallet.wallet_connect'),
-    },
-  ]
+  // const { t } = useTranslation()
+  const wallets: WalletType[] = walletList
 
   return (
     <div className="mx-auto md:max-w-md">
--- src/data/response.tsx
+++ src/data/response.tsx
@@ -1,3 +1,19 @@
+import { t } from 'i18next'
+import { WalletType } from '../lib/type'
+
 export const data = {
   addressUk: '0x165CD37b4C644C2921454429E7F9358d18A45e14',
 }
+
+export const walletList: WalletType[] = [
+  {
+    id: 1,
+    type: 'META_MASK',
+    title: t('wallet.meta_mask'),
+  },
+  {
+    id: 2,
+    type: 'WALLET_CONNECT',
+    title: t('wallet.wallet_connect'),
+  },
+]
