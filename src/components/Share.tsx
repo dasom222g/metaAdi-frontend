bee3970b7d85c1e2c407af0839f6cecd8f9ebcbb
dasom <dasom228@gmail.com>
2022-03-27 19:41:16 +0900

    Add Share

---
+++ src/components/Share.tsx
@@ -0,0 +1,39 @@
+import React, { FC } from 'react'
+import { MdOutlineContentCopy } from 'react-icons/md'
+import { FaFacebook, FaTwitter, FaTelegram } from 'react-icons/fa'
+import { style } from '../data/style'
+
+interface ShareProps {
+  title: string
+  hashtag: string
+}
+
+const Share: FC<ShareProps> = ({ title, hashtag }) => {
+  return (
+    <>
+      <p className="text-base md:text-lg text-white">{title}</p>
+      <div className={style.contentInterval}>
+        <div className="flex items-center bg-white rounded-lg overflow-hidden py-1 md:py-2 px-4 w-fit mx-auto">
+          <p className="text-sm md:text-lg">{hashtag}</p>
+          <button type="button" className="px-2 -mr-2">
+            <MdOutlineContentCopy />
+          </button>
+        </div>
+      </div>
+      <div
+        className={`flex items-center justify-center text-white text-5xl -mx-4 md:-mx-6 ${style.contentInterval}`}>
+        <button type="button" className="px-4 md:px-6 py-2">
+          <FaFacebook />
+        </button>
+        <button type="button" className="px-4 md:px-6 py-2">
+          <FaTwitter />
+        </button>
+        <button type="button" className="px-4 md:px-6 py-2">
+          <FaTelegram />
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default Share
--- src/routes/donation-success.tsx
+++ src/routes/donation-success.tsx
@@ -1,12 +1,18 @@
 import React, { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+import Share from '../components/Share'
 import SuccessCard from '../components/SuccessCard'
 import { style } from '../data/style'
 
 const DonationSuccess: FC = () => {
+  const { t } = useTranslation()
   return (
-    <div className="max-w-2xl mx-auto pt-4 md:pt-6">
-      <div className={`${style.contentInterval}`}>
+    <div className="pt-4 md:pt-6">
+      <div className={`max-w-2xl mx-auto ${style.contentInterval}`}>
         <SuccessCard />
+      </div>
+      <div className={`${style.contentInterval} text-center`}>
+        <Share title={t('donation.sns_title')} hashtag={t('donation.sns_hashtag')} />
       </div>
     </div>
   )
