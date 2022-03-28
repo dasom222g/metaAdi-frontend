66aabfb904de6b80e49136ccdf87a2170412900e
dasom <dasom228@gmail.com>
2022-03-28 15:19:13 +0900

    Fix style

---
+++ src/components/FireCracker.tsx
@@ -0,0 +1,83 @@
+import { CreateTypes } from 'canvas-confetti'
+import React, { FC, useCallback, useEffect, useRef } from 'react'
+import ReactCanvasConfetti from 'react-canvas-confetti'
+
+const canvasStyles = {
+  position: 'fixed',
+  pointerEvents: 'none',
+  width: '100%',
+  height: '100%',
+  top: 0,
+  left: 0,
+  zIndex: 10,
+} as React.CSSProperties
+
+// interface FireCrackerProps {
+//   isLoading: boolean | null
+// }
+
+export const FireCracker: FC = () => {
+  const refAnimationInstance = useRef<CreateTypes | null>(null)
+
+  const getInstance = useCallback((instance) => {
+    console.log('instance', typeof instance)
+    refAnimationInstance.current = instance
+  }, [])
+
+  const makeShot = useCallback((particleRatio, opts) => {
+    refAnimationInstance &&
+      refAnimationInstance.current &&
+      refAnimationInstance.current({
+        ...opts,
+        origin: { y: 0.7 },
+        particleCount: Math.floor(200 * particleRatio),
+      })
+  }, [])
+
+  const fire = useCallback(() => {
+    makeShot(0.25, {
+      spread: 46,
+      startVelocity: 55,
+      shapes: ['circle', 'square', 'square'],
+    })
+
+    makeShot(0.2, {
+      spread: 80,
+      shapes: ['circle', 'square', 'square'],
+    })
+
+    makeShot(0.35, {
+      spread: 120,
+      decay: 0.91,
+      scalar: 0.8,
+      shapes: ['circle', 'square', 'square'],
+    })
+
+    makeShot(0.1, {
+      spread: 140,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2,
+      shapes: ['circle', 'square', 'square'],
+    })
+
+    makeShot(0.1, {
+      spread: 140,
+      startVelocity: 45,
+      shapes: ['circle', 'square', 'square'],
+    })
+  }, [makeShot])
+
+  useEffect(() => {
+    setTimeout(() => {
+      fire()
+    }, 500)
+  }, [fire])
+
+  // view
+  return (
+    <>
+      <ReactCanvasConfetti refConfetti={getInstance} style={canvasStyles} />
+    </>
+  )
+}
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -33,7 +33,7 @@ const Header: FC = () => {
       <div className="py-4 px-4 md:py-6 md:px-8 fixed top-0 left-0 right-0 z-20 bg-aid-purple">
         <nav className="relative flex items-center justify-between" aria-label="Global">
           <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-            <div className="flex items-center justify-between w-full md:w-auto">
+            <div className="flex items-center justify-between w-full md:w-auto relative z-30">
               <Link to="/">
                 <img
                   className="h-8 w-auto md:h-10"
@@ -89,22 +89,25 @@ const Header: FC = () => {
             leaveTo="opacity-0 scale-0">
             <Popover.Panel
               focus
-              className="popup absolute z-20 top-0 inset-x-0 transition transform origin-top-right md:hidden p-0">
-              <div className="rounded-lg shadow-md overflow-hidden">
+              className="popup fixed z-20 inset-0 py-4 px-4 md:py-6 md:px-8 bg-aid-purple transition transform origin-top-right md:hidden">
+              <div className="overflow-hidden text-white bg-aid-purple">
                 <div className="text-right">
                   <Popover.Button className="rounded-md p-2 pr-0 inline-flex items-center justify-center text-white bg-aid-purple">
                     <span className="sr-only">Close main menu</span>
                     <XIcon className="h-6 w-6" aria-hidden="true" />
                   </Popover.Button>
                 </div>
-                <div className="px-2 pt-2 pb-3 space-y-1 bg-white bg-opacity-90">
+                <div className="px-2 pt-2 pb-3 space-y-1 text-center">
                   {navigation.map((item) => (
-                    <Link
-                      key={item.name}
-                      to={item.link}
-                      className="block px-3 py-2 rounded-md text-base font-bold text-gray-700">
-                      {item.name}
-                    </Link>
+                    <div key={item.name}>
+                      <Popover.Button>
+                        <Link
+                          to={item.link}
+                          className="block px-3 py-2 rounded-md text-lg font-bold">
+                          {item.name}
+                        </Link>
+                      </Popover.Button>
+                    </div>
                   ))}
                 </div>
                 {/* <button
