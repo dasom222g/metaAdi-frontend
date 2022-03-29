e0c1c8f2c8d5e875d2115a7da56f1b51ae8d4529
dasom <dasom228@gmail.com>
2022-03-29 13:57:02 +0900

    Fix main style

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -10,7 +10,7 @@ const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className="h-full wrap overflow-hidden">
       <Header />
-      <div className="h-full overflow-auto bg-aid-purple flex flex-col">
+      <div className="h-full overflow-auto overflow-x-hidden bg-aid-purple flex flex-col">
         <main className="pt-18 md:pt-22 pb-4 md:pb-6 px-4 md:px-8">{children}</main>
         <Footer />
       </div>
