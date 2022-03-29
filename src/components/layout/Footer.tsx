3410c76c4127c369469e984707b2af412ef355cb
dasom <dasom228@gmail.com>
2022-03-29 23:30:24 +0900

    Fix footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ const Header: FC = () => {
   return (
     <footer className="py-4 md:py-6 mt-auto px-8 bg-gray-800">
       <div className="max-w-2xl mx-auto">
-        <p className="text-white text-sm md:text-lg">Powered by somi</p>
+        <p className="text-white text-sm md:text-lg">Powered by Meta Aid Team</p>
       </div>
     </footer>
   )
