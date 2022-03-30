94174b112d6073797c183de3a23626f0c8a9b1cd
dasom <dasom228@gmail.com>
2022-03-30 18:16:11 +0900

    Fix metamask wallet connect

--- src/components/WalletBox.tsx
+++ src/components/WalletBox.tsx
@@ -9,17 +9,24 @@ import WalletBoxButton from './WalletBoxButton'
 const WalletBox: FC = () => {
   const navigate = useNavigate()
   // const {chainId, account, active, activate, deactivate} = useWeb3React()
-  const { account, activate, error } = useWeb3React()
+  const { active, account, activate, error } = useWeb3React()
 
   const wallets: WalletType[] = walletList
 
   const connectWallet = (type: keyof typeof Wallet): void => {
+    console.log('active', active)
+    // active && goDonation()
     type === Wallet.META_MASK ? metaMaskConnect() : walletConnect()
   }
 
   const metaMaskConnect = async (): Promise<void> => {
     try {
       await activate(injected)
+      // await activate(injected, (error) => {
+      //   if('/No Ethereum provider was found on window.ethereum/'.test(error)){
+      //     window.open('https://metamask.io/download.html');
+      //   }
+      // })
     } catch (error) {
       console.error(error)
     }
@@ -33,8 +40,9 @@ const WalletBox: FC = () => {
   }, [navigate])
 
   useEffect(() => {
-    account && goDonation()
-  }, [account, goDonation])
+    if (!active) return
+    goDonation()
+  }, [account, active, goDonation])
 
   useEffect(() => {
     error && console.log('error', error.message)
